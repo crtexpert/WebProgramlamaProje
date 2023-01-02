7f66da3fa94372539ad8f4d18de637f3f5690bf0
Görkem Kahyaoğlu <gorkem.kahyaoglu@ogr.sakarya.edu.tr>
2023-01-02 12:53:09 +0300

    update

--- WebProgramlamaProje/Controllers/YorumController.cs
+++ WebProgramlamaProje/Controllers/YorumController.cs
@@ -16,5 +16,23 @@ namespace WebProgramlamaProje.Controllers
             var deger = ctx.Yorumlar.ToList();
             return View(deger);
         }
+
+       
+        [HttpPost]
+        public ActionResult YorumAt(string yorumText,int postid)
+        {
+            int userID = Convert.ToInt32(Session["Id"]);
+            if(userID == 0)
+            {
+                return RedirectToAction("Giris", "Hesap");
+            }
+            Yorum yrm = new Yorum();
+            yrm.Mesaj = yorumText;
+            yrm.UserID = userID;
+            yrm.PostID = postid;
+            ctx.Yorumlar.Add(yrm);
+            ctx.SaveChanges();
+            return RedirectToAction("Index", "Anasayfa");
+        }
     }
 }
